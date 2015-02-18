c8aa4dd343437b416151d012abb9dadb8ccbb0b8
Othniel VISKY <ovisky@e1p5.cluj.42.fr>
2015-02-18 08:57:58 +0100

    pui pui

--- WORK_puissance/puissance4.c
+++ WORK_puissance/puissance4.c
@@ -1,13 +1,32 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ouissance4.c                                       :+:      :+:    :+:   */
+/*   puissance4.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ovisky <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/02/18 08:12:39 by ovisky            #+#    #+#             */
-/*   Updated: 2015/02/18 08:13:37 by ovisky           ###   ########.fr       */
+/*   Updated: 2015/02/18 08:57:26 by ovisky           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ouissance.h"
 
+int		main(int ac, char **av)
+{
+	char	*line;
+	int		col;
+	int		row;
+	int		turn;
+	int		max_turn;
+
+	max_turn = col * row;
+	while (turn != max_turn)
+	{
+		print_board();
+		if (turn % 2 == 0)
+			player.play();
+		else
+			comp.play();
+	}
+}
--- WORK_puissance/puissance4.h
+++ WORK_puissance/puissance4.h
@@ -6,8 +6,25 @@
 /*   By: ovisky <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/02/18 08:12:28 by ovisky            #+#    #+#             */
-/*   Updated: 2015/02/18 08:12:30 by ovisky           ###   ########.fr       */
+/*   Updated: 2015/02/18 08:57:19 by ovisky           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef PUISSANCE_H
+# define PUISSANCE_H
 
+typedef	struct	s_grid
+{
+		int		x;
+		int		y;
+
+		int		**board;
+}				t_boeard;
+
+typedef	struct	s_player
+{
+		char	piece;
+		int		turn;
+}				t_player;
+
+#endif
