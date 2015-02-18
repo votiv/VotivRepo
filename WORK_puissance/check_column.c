54e6cac5f3782f017eda42ca6b07082c843c1e28
Othniel VISKY <ovisky@e1p5.cluj.42.fr>
2015-02-18 18:39:45 +0100

    stuff

--- WORK_puissance/check_column.c
+++ WORK_puissance/check_column.c
@@ -1,37 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   puissance4.h                                       :+:      :+:    :+:   */
+/*   check_column.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ovisky <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2015/02/18 08:12:28 by ovisky            #+#    #+#             */
-/*   Updated: 2015/02/18 18:38:50 by ovisky           ###   ########.fr       */
+/*   Created: 2015/02/18 18:25:46 by ovisky            #+#    #+#             */
+/*   Updated: 2015/02/18 18:38:48 by ovisky           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PUISSANCE_H
-# define PUISSANCE_H
+#include "puissance.h"
 
-typedef	struct	s_board
+int		check_column(char *line)
 {
-		int			col;
-		int			row;
+	if (!is_col_ok(line))
+	{
+		ft_putendl("Enter the number of the column you want to play!");
+		return (-1);
+	}
 
-		int			**board;
-}					t_board;
-
-typedef	struct		s_player
-{
-		char		piece;
-}					t_player;
-
-typedef	struct		s_state
-{
-		t_board		board;
-		t_player	human;
-		t_player	comp;
-		int			done;
-}					t_state;
-
-#endif
+}
--- WORK_puissance/puissance4.c
+++ WORK_puissance/puissance4.c
@@ -6,27 +6,72 @@
 /*   By: ovisky <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/02/18 08:12:39 by ovisky            #+#    #+#             */
-/*   Updated: 2015/02/18 08:57:26 by ovisky           ###   ########.fr       */
+/*   Updated: 2015/02/18 18:25:58 by ovisky           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ouissance.h"
+#include "puissance.h"
+#include <stdlib.h>
+
+void	print_board()
+{
+
+}
+
+int		go_human()
+{
+	char	*line;
+	int		ret;
+
+	ret = -1;
+	print_board(state->board);
+	ft_putstr("It's your turn, enter a column ");
+	if (state->human.piece == 'O')
+		ft_putendl("O: ");
+	else
+		ft_putendl("X: ");
+	while (ret != 1 && get_next_line(0, &line) > 0)
+	{
+		ret = check_column(line); // itt vagyok 18/02___19:25
+		if (ret == -2)
+			return (-1);
+		free(line);
+	}
+	return (1);
+}
+
+int		go_play(t_state state)
+{
+	char	c;
+	char	piece;
+
+	if (check_full(state->board))
+	{
+		print_board(state->board);
+		ft_putendl("It's a tie!");
+		return (-1);
+	}
+	// some error checking on the players maybe?
+	if (state->turn == 0)
+		if(go_comp() == -1)
+			return (-1);
+	if(state->turn == 1)
+		if (go_human() == -1)
+			return (-1);
+	c = (state->turn == 0) ? state->comp.piece : state->human.piece;
+	if ((piece = check_win(state->board, c, 4)) != ' ')
+	{
+		print_board(state->board);
+		// who won?
+		return (-1);
+	}
+	state->turn = (state->turn == 0) ? 1 : 0;
+}
 
 int		main(int ac, char **av)
 {
+	t_state	state;
 	char	*line;
-	int		col;
-	int		row;
-	int		turn;
-	int		max_turn;
 
-	max_turn = col * row;
-	while (turn != max_turn)
-	{
-		print_board();
-		if (turn % 2 == 0)
-			player.play();
-		else
-			comp.play();
-	}
+	go_play(state);
 }
