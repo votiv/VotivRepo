e5681a4924c72e39879c53e301b0bd1c36b360ef
Othniel VISKY <ovisky@e1p5.cluj.42.fr>
2015-03-04 18:18:04 +0100

    Libft.

---
+++ Libft/ft_memmove.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ovisky <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2014/11/13 13:36:37 by ovisky            #+#    #+#             */
+/*   Updated: 2014/11/13 13:36:38 by ovisky           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memmove(void *dest, const void *src, size_t n)
+{
+	unsigned char	tmp[n];
+	
+	ft_memcpy(tmp, src, n);
+	ft_memcpy(dest, tmp, n);
+	return (dest);
+}
--- Libft/libft.h
+++ Libft/libft.h
@@ -25,5 +25,16 @@ char	*ft_strsub(char const *s, unsigned int start, size_t len);
 char	*ft_strtrim(char const *s);
 int		ft_isspace(char c);
 char	*ft_strcpy(char *dest, const char *src);
+void	ft_bzero(void *s, size_t n);
+int		ft_isalnum(int c);
+int		ft_isprint(int c);
+int		ft_isdigit(int c);
+int		ft_isdascii(int c);
+int		ft_isalpha(int c);
+void	*ft_memccpy(void *dest, void *src, int c, size_t n);
+void	*ft_memchr(const void *str, int c, size_t n);
+int		ft_memcmp(const void *s1, const void *s2, size_t n);
+void	*ft_memcpy(void *dest, const void *src, size_t n);
+void	*ft_memset(void *b, int c, size_t len);
 
 #endif
