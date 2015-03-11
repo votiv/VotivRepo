2b2d630ae71c2eabec7297e90be46b3931ca0372
Visky Othniel <othniel.visky@pitechnologies.ro>
2015-03-11 10:14:24 +0200

    Finished Libft?.

---
+++ Libft/ft_lstmap.c
@@ -0,0 +1,23 @@
+#include "libft.h"
+
+t_list	*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem))
+{
+	t_list	*ret;
+	t_list	*ret_first;
+	t_list	*ret_prev;
+
+	ret_first =NULL;
+	while (lst != NULL)
+	{
+		ret = (t_list *)ft_memalloc(sizeof(*ret));
+		if (ret == NULL)
+			return (NULL);
+		if (ret_first == NULL)
+			ret_first = ret;
+		ft_memcpy(ret, (*f)(lst), sizeof(t_list));
+		ret_prev->next = ret;
+		ret_prev = ret;
+		lst = lst->next;
+	}
+	return (ret_first);
+}
