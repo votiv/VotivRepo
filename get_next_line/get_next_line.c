790072e466143314c5d0f09120b197f7c6defa15
Othniel Visky <othniel.visky@pitechnologies.ro>
2016-02-02 10:52:48 +0200

    the struggle continues.

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -12,24 +12,92 @@
 
 #include "get_next_line.h"
 
-int		get_next_line(int fd, char **line)
-{	
-	int		ret;
-	char	*tmp;
+static void			append_line(char **str, char **s_str)
+{
+	char	*tmp1;
+	char	*tmp2;
 
-	tmp = (char *) ft_memalloc(sizeof(*tmp) * BUFF_SIZE);
-	ret = read(fd, *line, 8);
-	if (ret != -1)
+	tmp2 = NULL;
+	tmp1 = NULL;
+	if (*str && **str)
 	{
-		tmp = ft_strcat(tmp, *line);
-	}
-	while ((ret = read(fd, *line, 1)) != -1)
-	{
-		if (**line != '\n' || **line != '\0')
+		tmp1 = ft_strdup(*str);
+		if (*s_str)
 		{
-			tmp = ft_strjoin(tmp, *line);
+			tmp2 = ft_strdup(*s_str);
+			free(*s_str);
 		}
+		*s_str = ft_strjoin(tmp2, tmp1);
+		if (tmp2)
+			free(tmp2);
+		if (tmp1)
+			free(tmp1);
 	}
-	printf("line: %s, ret: %d\n", tmp, ret);
+}
+
+static unsigned int	check_line(char *s_str)
+{
+	unsigned int i;
+
+	i = 0;
+	while (s_str[i] != '\0')
+	{
+		if (s_str[i] == '\n')
+		{
+			s_str[i] = '\0';
+			return (i + 1);
+		}
+		i++;
+	}
+	return (i);
+}
+
+static int			return_line(char **str, char **s_str, char **line)
+{
+	unsigned int	i;
+	char			*tmp;
+
+	tmp = NULL;
+	i = check_line(*s_str);
+	*line = ft_strdup(*s_str);
+	tmp = ft_strdup(*s_str + i);
+	free(*s_str);
+	*s_str = tmp;
+	if (str)
+		free(*str), *str = NULL;
+	return (1);
+}
+
+int					get_next_line(int fd, char **line)
+{	
+	int			ret;
+	char		*tmp;
+	static char	*s_tmp;
+
+	tmp = NULL;
+	s_tmp = NULL;
+	if (fd <= 0 || (!(line)))
+		return (-1);
+	if ((!(tmp = (char *) ft_memalloc(sizeof(*tmp) * BUFF_SIZE))))
+		return (-1);
+	while (((ret = read(fd, tmp, BUFF_SIZE)) > 0) && (!(ft_strchr(tmp, '\n'))))
+	{
+		tmp[ret] = '\0';
+		append_line(&tmp, &s_tmp);
+	}
+	if (tmp && *tmp && ret > 0)
+	{
+		tmp[ret] = '\0';
+		append_line(&tmp, &s_tmp);
+	}
+	if (ret < 0)
+		return (-1);
+	if (s_tmp && s_tmp[0] != '\0')
+		return (return_line(&tmp, &s_tmp, line));
+	if (tmp)
+		free(tmp), tmp = NULL;
+	free(s_tmp);
+	s_tmp = NULL;
+	*line = NULL;
 	return 0;
 }
--- get_next_line/main.c
+++ get_next_line/main.c
@@ -4,17 +4,19 @@
 
 int		main() {
 	
-	int		fd;
+	int		fd, ret;
 	char	*line;
 	
 	line = (char *) ft_memalloc(sizeof(*line) * BUFF_SIZE);
 	fd = open("./filename.txt", O_RDONLY);
-	if (fd < 0) {
+	if (fd < 0)
+	{
 		printf("Error when opening file!");
 		return (-1);
 	}
 	/*printf("funk: %d\n", get_next_line(fd, &line));*/
-	while (get_next_line(fd, &line) != 0) {
+	while ((ret = get_next_line(fd, &line)) != 0)
+	{
 		printf("%s", line);
 	}
 	return 0;
