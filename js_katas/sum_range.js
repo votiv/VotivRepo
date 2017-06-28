ea2886915eea5542c3d0f6039eb6de9de4e5f6a8
Othniel Visky <othniel.visky@pitechnologies.ro>
2017-06-28 17:10:17 +0300

    continued with katas from eloquent javascript

--- js_katas/sum_range.js
+++ js_katas/sum_range.js
@@ -1,21 +1,58 @@
 
+function reverse(array) {
+
+	let copy = [],
+			i = array.length - 1
+
+	while (i >= 0) {
+
+		copy.push(array[i])
+		--i
+	}
+
+	return copy
+}
+
 function range(start, end, step) {
 
-	var ret = [],
-			$s, $e;
+	let ret = [],
+			rev = false,
+			$start,
+			$end
 
 	if (start === end) return start
 
-	if (!!step && step < 0) {
-		$s = end;
-		$e = start;
+	if (start < end) {
+		$start = start
+		$end = end
+	} else {
+		$start = end
+		$end = start
 	}
 
-	return ret;
+	if (!!step && step < 0) {
+		rev = true
+		step *= -1
+	}
+
+	while ($start <= $end) {
+
+		ret.push($start)
+
+		if (!!step) {
+			$start += step
+		} else {
+			++$start
+		}
+	}
+
+	return rev ? reverse(ret) : ret
 }
+
 
 console.log(range(1, 10));
 console.log(range(10, 1));
 console.log(range(10, 1, 2));
 console.log(range(1, 10, 2));
 console.log(range(5, 2, -1));
+console.log(range(1, 10, -2));
